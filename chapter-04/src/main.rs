1162b82f93cdc8c1ffd7c3548f2bdb806ace0a49
Tangjj1996 <tangjj_1996@163.com>
2021-08-01 20:59:25 +0800

    feat: fortunately complete fabonacci

--- chapter-04/src/main.rs
+++ chapter-04/src/main.rs
@@ -1,21 +1,28 @@
 use std::io;
 
 fn main() {
-    println!("please enter your fibonacci nth: ");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("faild to read line");
-
-    let guess: i32 = match guess.trim().parse() {
-        Ok(num) => num,
-        Err(_) => 0,
-    };
-    println!("you fibonacci list is:: {}", fibonacci(guess));
+    loop {
+        println!("please enter your fibonacci nth: ");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("faild to read line");
+        if guess.trim() == "quit" {
+            break;
+        };
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("you fibonacci list is:: {}", fibonacci(guess));
+        println!("please enter ctrl+c or put \'quit\' to end")
+    }
 }
 
 fn fibonacci(_nth: i32) -> i32 {
-    1
+    match _nth {
+        0 => 1,
+        1 => 1,
+        _ => fibonacci(_nth - 1) + fibonacci(_nth - 2),
+    }
 }
